a9387b0683a204d94fc68efcd94196042dfe06c5
SBT-Selin-AN <anselin.sbt@sberbank.ru>
2016-05-10 16:50:57 +0400

    Fixed python bot

--- example/py/Bot.py
+++ example/py/Bot.py
@@ -1,7 +1,7 @@
 import random
 
-cross = 'X'
-zero = 'O'
+cross = 'x'
+zero = 'o'
 empty = '-'
 
 def getMySymbol():
@@ -13,11 +13,16 @@ def getPlaceToStep():
     while True:
         place = random.randint(0, len(field) - 1)
         if field[place] == empty:
-            return place;
+            return place
+        
+def doStep():
+    return field[0:place] + mySymbol + field[place + 1:]
 
-sourceField = input()
-field = sourceField.upper()
-mySymbol = getMySymbol()
-place = getPlaceToStep()
-outField = sourceField[0:place] + mySymbol + sourceField[place + 1]
-print(outField)
+while True:
+    field = input()
+    if field.count(empty) == 0:
+        break
+    mySymbol = getMySymbol()
+    place = getPlaceToStep()
+    print(doStep())
+print('Game over')
